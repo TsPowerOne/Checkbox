ae0f63a869b50a3d282c4213da4999f16f4405e3
luigisenzamici <luigisenzamici@outlook.it>
2018-10-16 17:32:14 +0200

    isEnabled, enable, disable, check and uncheck methods added, enabled property added

--- lib/checkbox.d.ts
+++ lib/checkbox.d.ts
@@ -1,19 +1,29 @@
 export declare class CheckBox {
     private Root;
     private name;
-    private checked?;
-    private exclusive?;
     private group?;
     private Id?;
     private Class?;
     private Style?;
+    private checked;
+    private exclusive;
+    private disabled;
     private node;
     private clicked;
     private changed;
+    private enab;
+    private disab;
     clicked$: import("rxjs/internal/Observable").Observable<any>;
     changed$: import("rxjs/internal/Observable").Observable<any>;
-    constructor(Root: HTMLElement, name: string, checked?: boolean, exclusive?: boolean, group?: string, Id?: string, Class?: string, Style?: string);
+    enabled$: import("rxjs/internal/Observable").Observable<any>;
+    disabled$: import("rxjs/internal/Observable").Observable<any>;
+    constructor(Root: HTMLElement, name: string, group?: string, Id?: string, Class?: string, Style?: string, checked?: boolean, exclusive?: boolean, disabled?: boolean);
     create: () => void;
     isChecked: () => boolean;
+    check: () => void;
+    uncheck: () => void;
+    isEnabled: () => boolean;
+    enable: () => void;
+    disable: () => void;
     private NodeSelect;
 }
--- src/checkbox.ts
+++ src/checkbox.ts
@@ -6,37 +6,51 @@ export class CheckBox{
     private node:HTMLInputElement;
     private clicked = new Subject<any>();
     private changed = new Subject<any>();
+    private enab = new Subject<any>();
+    private disab = new Subject<any>();
+
     public clicked$ = this.clicked.asObservable();
     public changed$ = this.changed.asObservable();
-    
+    public enabled$ = this.enab.asObservable();
+    public disabled$ = this.disab.asObservable();
     
 
-    constructor(private Root:HTMLElement,
+    constructor(
+                private Root:HTMLElement,
                 private name:string,
-                private checked?:boolean,
-                private exclusive?:boolean,
+
                 private group?:string,
                 private Id?:string, 
                 private Class?:string, 
-                private Style?:string)
+                private Style?:string,
+
+                private checked:boolean = false,
+                private exclusive:boolean = false,
+                private disabled:boolean = false
+                )
     {
         this.create();
     }
     
-    create = ()=>{
+    create = () => {
         let that = this;
+        
         let input = document.createElement("input");
         input.setAttribute("type", "checkbox");
         input.setAttribute("name", this.name);
 
         if(this.group)input.setAttribute("data-group", this.group);
-
         if(this.checked)input.setAttribute("checked", "checked");
+        if(this.disabled)input.setAttribute("disabled", "disabled");
+
+
+
 
         input.addEventListener("click", (event)=>{
             this.clicked.next( { name:this.name } );
             
         });
+
         input.addEventListener("change", (event)=>{
 
             if(this.node.getAttribute("checked")){
@@ -69,16 +83,45 @@ export class CheckBox{
         });
 
 
+
+
         if(this.Id)input.setAttribute("id", this.Id);
         if(this.Class)input.setAttribute("class", this.Class);
         if(this.Style)input.setAttribute("style", this.Style);
         this.node = input;
         this.Root.appendChild(input);
+
+
     }
-    isChecked = ():boolean=>{
-        let ck = this.node.getAttribute("checked");
-        this.checked = (ck)?true:false;
+    isChecked = ():boolean => {
+        this.checked = (this.node.getAttribute("checked"))?true:false;
         return this.checked;
+    }
+    check = ():void => {
+                this.checked = true;
+                this.node.setAttribute("checked", "checked");
+                this.node.checked = true;
+                this.changed.next( { name:this.name, checked:this.checked } );
+    }
+    uncheck = ():void => {
+        this.checked = false;
+        this.node.removeAttribute("checked");
+        this.node.checked = false;
+        this.changed.next( { name:this.name, checked:this.checked } );
+    }
+    isEnabled = ():boolean =>{
+        this.disabled = (this.node.getAttribute("disabled"))? true : false;
+        return !this.disabled;
+    }
+    enable = ():void =>{
+        this.node.disabled = false;
+        this.node.removeAttribute("disabled");
+        this.enab.next( { name:this.name, enabled: true } );
+    }
+    disable = ():void =>{
+        this.node.disabled = true;
+        this.node.setAttribute("disabled", "disabled");
+        this.disab.next( { name:this.name, enabled: false } );
     }
     private NodeSelect = (selector:string):Array<any>=>{
         let nodes = document.querySelectorAll(selector);
